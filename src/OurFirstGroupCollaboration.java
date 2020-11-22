06ba513091cdf30f365e447022fd96b0990c1ed6
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-22 17:34:20 -0500

    I created a forLoop to start the solution to the problem

--- src/OurFirstGroupCollaboration.java
+++ src/OurFirstGroupCollaboration.java
@@ -3,7 +3,12 @@ public class OurFirstGroupCollaboration {
     public static void main(String[] args) {
 //Hi Group! I'm inviting you all to collaborate on a project to solve the Java Program below:
         //. write a program that can print all the ODD numbers between 0 ~ 100 that can be divisible by 3 & 5
+//I will begin working on the solution
 
+        for(int i =0; i<=100; i++){
+
+
+        }
 
     }
 }
