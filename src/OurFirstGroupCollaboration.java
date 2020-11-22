2d4557bcb3030f2868f5fd5d64ea0bfa881ee5cc
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-22 17:30:27 -0500

    Initial commit: I'm inviting each of you to collaborate on this project.

---
+++ src/OurFirstGroupCollaboration.java
@@ -0,0 +1,13 @@
+public class OurFirstGroupCollaboration {
+
+    public static void main(String[] args) {
+//Hi Group! I'm inviting you all to collaborate on a project to solve the Java Program below:
+        //. write a program that can print all the ODD numbers between 0 ~ 100 that can be divisible by 3 & 5
+
+
+    }
+}
+/*
+. write a program that can print all the ODD numbers between 0 ~ 100 that can be divisible by 3 & 5
+
+ */
